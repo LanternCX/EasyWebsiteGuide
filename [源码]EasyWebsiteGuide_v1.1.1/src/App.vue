c4bfd65c942380e3d01031bfc05ff88ef8cff9fe
CDBxinhe <76872604+CDBxinhe@users.noreply.github.com>
2022-02-27 17:32:31 +0800

    v1.1.1

---
+++ [源码]EasyWebsiteGuide_v1.1.1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <guide-block></guide-block>
+    <search-block></search-block>
+  </div>
+</template>
+
+<script>
+//搜索框模块
+import SearchBlock from '@/components/SearchBlock/SearchBlock.vue'
+//书签框模块
+import GuideBlock from '@/components/GuideBlock/GuideBlock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchBlock,
+    GuideBlock
+  }
+}
+
+</script>
+
+<style>
+#app {
+  width: 100%;
+  min-width: 550px;
+  height: 100%;
+  min-height: 540px;
+  background: url("./assets/bg.png") no-repeat;
+  background-size: cover;
+  text-align: center;
+  color: #2c3e50;
+  overflow: hidden;
+}
+</style>
---
+++ [源码]EasyWebsiteGuide_v1.1.1/src/components/SearchBlock/EngineSwitch.vue
@@ -0,0 +1,72 @@
+<template>
+  <input
+    type="button"
+    class="engineSwitch"
+    ref="engineSwitch"
+    :id="this.engineName"
+    :value="this.engineName"
+    @click="changeSearchEngineAndStyle(engineName)"
+    :style="btnStyle"
+  >
+
+</template>
+
+<script>
+export default {
+  name: 'EngineSwitch',
+  props: [
+    'engineName',
+    'changeSearchEngine'
+  ],
+  data() {
+    return {
+      btnStyle: '',
+    };
+  },
+  mounted() {
+    this.$bus.$on('btnStyle')
+  },
+  methods: {
+    changeSearchEngineAndStyle(engineName) {
+      this.changeSearchEngine(engineName)
+    }
+  }
+}
+</script>
+
+<style>
+.engineSwitch {
+  height: 30px;
+  width: 70px;
+  border-style: none;
+  margin-top: 5px;
+  margin-right: 5px;
+  opacity: 0.7;
+  border-radius: 150px;
+  background-color: #2f3640;
+  color: #f4f4f4;
+  transition-duration: 0.5s;
+  animation: slide-in-blurred-top-2 0.6s cubic-bezier(0.23, 1, 0.32, 1) both;
+}
+.engineSwitch:hover {
+  box-shadow: 0 10px 20px 0px rgba(1, 1, 1);
+  opacity: 0.95;
+}
+.engineSwitch:active {
+  background-color: #7f8c8d;
+}
+@keyframes slide-in-blurred-top-2 {
+  0% {
+    transform: translateY(-50px) scaleY(2.5) scaleX(0.2);
+    transform-origin: 50% 0%;
+    filter: blur(40px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0) scaleY(1) scaleX(1);
+    transform-origin: 50% 50%;
+    filter: blur(0);
+    opacity: 0.7;
+  }
+}
+</style>
